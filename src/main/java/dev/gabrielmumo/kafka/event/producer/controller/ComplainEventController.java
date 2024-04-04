05c33ce75207a5c7d332fe17a1e8776ead66f1ab
Gabriel Munera <gabrielmumo@MacBook-Pro-de-Gabriel.local>
2024-04-03 19:40:05 -0500

    Logs event in the controller

--- src/main/java/dev/gabrielmumo/kafka/event/producer/controller/ComplainEventController.java
+++ src/main/java/dev/gabrielmumo/kafka/event/producer/controller/ComplainEventController.java
@@ -1,6 +1,8 @@
 package dev.gabrielmumo.kafka.event.producer.controller;
 
 import dev.gabrielmumo.kafka.event.producer.dto.ComplainEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ComplainEventController {
 
+    Logger log = LoggerFactory.getLogger(ComplainEventController.class);
+
     @PostMapping("/v1/complains")
     public ResponseEntity<ComplainEvent> putComplainEvent(@RequestBody ComplainEvent complainEvent) {
+        log.info("ComplainEvent: {} ", complainEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(complainEvent);
     }
